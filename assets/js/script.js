548ff7d2201ee73bf603079e29dcb864c0fc638c
HamzaR19 <m.hamza.rahman@gmail.com>
2023-05-30 23:47:33 -0500

    finishing JS and psuedo

--- assets/js/script.js
+++ assets/js/script.js
@@ -86,3 +86,95 @@ function convertToFahrenheit(kelvin) {
     var currentWind = document.getElementById("current-wind");
     currentWind.textContent = "Wind: " + weather.wind.speed + " MPH";
   }
+
+  // Loop
+function setForecast(forecast) {
+    for (var i = 0; i < forecast.length; i += 8) {
+      console.log(forecast[i]);
+      setForecastDay(forecast[i], i / 8 + 1);
+    }
+  }
+  
+  // Function 5 day forecast
+  function setForecastDay(weather, dayNumber) {
+
+
+    // image icons
+    var iconParagraph = document.createElement("p");
+    var iconParagraphId = weather.weather[0].icon;
+    var iconLink =
+      "https://openweathermap.org/img/wn/" + iconParagraphId + ".png";
+    console.log(iconLink);
+    var iconHTML = '<img src="' + iconLink + '">';
+    console.log(iconHTML);
+    
+    
+    // convert millisecs
+    var forecastParagraph = document.createElement("p");
+    var forecastDate = weather.dt;
+    console.log(forecastDate);
+    var convertTimeMilli = forecastDate * 1000;
+    var dateTime = new Date(convertTimeMilli);
+    var newDate = dateTime.toLocaleDateString("en-US", { dateStyle: "short" });
+    console.log(newDate);
+   
+   
+    // Attached to page
+    var day = document.getElementById("day-" + dayNumber);
+    var dayList = [];
+    var titleParagraph = document.createElement("p");
+    console.log(iconHTML);
+    titleParagraph.innerHTML = newDate + iconHTML;
+    dayList.push(titleParagraph);
+    
+    // Humidity
+    var humidityParagraph = document.createElement("p");
+    humidityParagraph.textContent = "Humidity: " + weather.main.humidity + " %";
+    dayList.push(humidityParagraph);
+    day.replaceChildren(...dayList);
+    
+    // Wind
+    var windParagraph = document.createElement("p");
+    windParagraph.textContent = "Wind: " + weather.wind.speed + " MPH";
+    dayList.push(windParagraph);
+    day.replaceChildren(...dayList);
+
+    // Temp
+    var tempParagraph = document.createElement("p");
+    tempParagraph.textContent = "Temp: " + weather.main.temp + " \xB0F";
+    dayList.push(tempParagraph);
+    day.replaceChildren(...dayList);
+    
+  }
+  
+  // Submit Eventlist
+  var searchForm = document.getElementById("search-form");
+  var city = document.getElementById("city");
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var searchCity = city.value.trim();
+   
+    //Local Storage for searched cities
+    executeSearch(searchCity);
+   
+    // Button for recently searched city
+    addRecentSearch(searchCity);
+  });
+  
+  
+  function addRecentSearch(city) {
+    
+    var recentButton = document.createElement("button");
+    recentButton.textContent = city;
+    recentButton.addEventListener("click", function () {
+      executeSearch(city);
+    });
+    searchHistory.appendChild(recentButton);
+  }
+  
+  // Local Storage
+  function executeSearch(searchCity) {
+    recentSearches.push(searchCity);
+    localStorage.setItem("searches", JSON.stringify(recentSearches));
+    getWeather(searchCity);
+  }
