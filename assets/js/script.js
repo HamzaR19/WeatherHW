9a8ccac0229d04351a4cc346c842a23aed6d1362
HamzaR19 <m.hamza.rahman@gmail.com>
2023-05-30 23:23:31 -0500

    added more js

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,55 @@ function getWeather(city) {
       setForecast(data.list);
     });
 }
+
+
+// K to F function
+function convertToFahrenheit(kelvin) {
+    var kTemp = kelvin;
+    var kToFar = (kTemp - 273.15) * 1.8 + 32;
+    var message = kToFar + " \xB0F";
+    return message;
+  }
+  console.log(convertToFahrenheit(279.45));
+  
+  function setCurrentWeather(weather) {
+    
+    
+    // Function to convert time
+    var forecastDate = weather.dt;
+    console.log(forecastDate);
+    var convertTimeMilli = forecastDate * 1000;
+    var dateTime = new Date(convertTimeMilli);
+    var newDate = dateTime.toLocaleDateString("en-US", { dateStyle: "short" });
+    console.log(newDate);
+    var currentDate = document.getElementById("current-date");
+    currentDate.innerHTML = newDate;
+    
+    
+    // Set Icon
+    var iconParagraphId = weather.weather[0].icon;
+    var iconLink =
+      "https://openweathermap.org/img/wn/" + iconParagraphId + ".png";
+    console.log(iconLink);
+    var iconHTML = '<img src="' + iconLink + '">';
+    console.log(iconHTML);
+    var currentIcon = document.getElementById("current-icon");
+    currentIcon.innerHTML = iconHTML;
+    
+    
+    // Curremt temp
+    console.log(weather.main.temp);
+    var currentTemperature = document.getElementById("current-temperature");
+    var convert = convertToFahrenheit(weather.main.temp);
+    console.log(convert);
+    currentTemperature.textContent =
+      "Temperature: " + weather.main.temp + " \xB0F";
+    
+    // Current Humidity
+    var currentHumidity = document.getElementById("current-humidity");
+    currentHumidity.textContent = "Humidity: " + weather.main.humidity + " %";
+
+    // Current Wind
+    var currentWind = document.getElementById("current-wind");
+    currentWind.textContent = "Wind: " + weather.wind.speed + " MPH";
+  }
